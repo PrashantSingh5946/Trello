61cad159169afb1c44e6aa92752aa20a6ecee9e0
Prashant Singh <octanesingh@gmail.com>
2022-01-22 17:45:53 +0530

    Dragging enabled

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import "./App.css"
+import "./App.css";
 import Collection from "./components/Collection";
-
+import { DragDropContext } from "react-beautiful-dnd";
 
 function App() {
   return (
     <div className="App">
-      <div className="Collections-menu">
-
+      <div className="navbar"></div>
+      <div className="container">
+        <div className="collections-menu"></div>
+        <div className="content">
+        <DragDropContext onDragEnd={() => {}}>
+          <div className="board">
+            <Collection
+              name="All"
+              tasks={[
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+                { name: "Random task" },
+              ]}
+            ></Collection>
+          </div>
+          </DragDropContext>
+        </div>
       </div>
-      <div className="content">
-    <div className="board">
-    <Collection>
-      </Collection>
-    </div>
-      </div>
-      
     </div>
   );
 }
--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,23 +1,42 @@
 import React from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-export default function Collection() {
+export default function Collection({ tasks, name }) {
   return (
     <div className="collection">
       <div className="header">
-        <h2>All</h2>
+        <h2>{name}</h2>
         <div className="ellipsis">&#8230;</div>
       </div>
-      <div className="list">
-        <div className="list-card">Random content</div>
-        <div className="list-card">Random content</div>
-        <div className="list-card">Random content</div>
-        <div className="list-card">Random content</div>
-        <div className="list-card">Random content</div>
-        <div className="list-card">Random content</div>
-      </div>
-      <div className="addItem">
-        + Add a Card
-      </div>
+      <DragDropContext onDragEnd={() => {}}>
+        <Droppable droppableId={`100`}>
+          {(provided) => {
+            return (
+              <div
+                className="list"
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                {tasks.map((task, index) => (
+                  <Draggable key={index} draggableId={`id-${index}`} index={index}>
+                    {(provided) => (
+                      <div
+                        className="list-card"
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        {task.name+index}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+            );
+          }}
+        </Droppable>
+      </DragDropContext>
+      <div className="addItem">+ Add a Card</div>
     </div>
   );
 }
