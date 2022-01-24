25db2c14dfc2c99b05101cbbf8bd1ae489379672
Prashant Singh <octanesingh@gmail.com>
2022-01-24 20:51:41 +0530

    Changing modal background close fundtionality

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,27 +1,31 @@
-import React,{useRef} from 'react';
-import CloseIcon from '@mui/icons-material/Close';
+import React, { useRef } from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
-
-export default function Modal({submitHandler,closeHandler}) {
-    const nameRef = useRef();
-    function onSubmit(e)
-    {
-        e.preventDefault();
-        submitHandler(nameRef.current.value);
-        closeHandler();
-    }
-  return (<div className='modalBackdrop'>
-  <form className="addBoard" onSubmit={onSubmit}>
-     <div className="row">
-       <input type="text" placeholder="Board name" ref={nameRef} autoFocus></input>
-     </div>
-     <div className="row">
-         <button type="submit">Add Board</button>
-         <span className="close" onClick={closeHandler}><CloseIcon></CloseIcon></span>
-         
-     </div>
-   </form>
-
-</div>)
-  
+export default function Modal({ submitHandler, closeHandler }) {
+  const nameRef = useRef();
+  function onSubmit(e) {
+    e.preventDefault();
+    submitHandler(nameRef.current.value);
+    closeHandler();
+  }
+  return (
+    <div className="modalBackdrop" onClick={closeHandler}>
+      <form className="addBoard" onSubmit={onSubmit}>
+        <div className="row">
+          <input
+            type="text"
+            placeholder="Board name"
+            ref={nameRef}
+            autoFocus
+          ></input>
+        </div>
+        <div className="row">
+          <button type="submit">Add Board</button>
+          <span className="close" onClick={closeHandler}>
+            <CloseIcon></CloseIcon>
+          </span>
+        </div>
+      </form>
+    </div>
+  );
 }
